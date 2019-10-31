b05693c58478390f3bec16c40ed739ba63bf8a98
kabirnjib0@gmail.com <kabirnajib0@gmail.com>
2019-10-31 23:05:43 +0100

    spec

---
+++ spec/add-spec.js
@@ -0,0 +1,11 @@
+/* eslint-disable prefer-arrow-callback */
+/* eslint-disable no-undef */
+const app = require('./add.js');
+
+// eslint-disable-next-line no-undef
+describe('Addition', function () {
+  it('The function should add 2 numbers', function () {
+    const value = app.AddNumber(5, 6);
+    expect(value).toBe(11);
+  });
+});
