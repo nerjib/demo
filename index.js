6e107d2f695b362f9f996275f8cd845189773f61
kabirnjib0@gmail.com <kabirnajib0@gmail.com>
2019-11-03 03:21:18 +0100

    fjf fkf

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+/* eslint-disable no-console */
 const http = require('http');
 
 const hostname = '127.0.0.1';
@@ -7,7 +7,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Worl w nv g hh\n');
+  res.end('Hello Worl w nv g k hh\n');
 });
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
